69c1bf4a5d1ed1fb6fccdab3e7da9b720ab3f32f
cooboyno111 <158344825@qq.com>
2018-08-08 10:36:20 +0800

    add source code

---
+++ Donate.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="login-wrap">
+        <div class="ms-title">WFC捐赠页面</div>
+        <div class="ms-login">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
+                <el-form-item prop="amount">
+                    <el-input v-model="ruleForm.amount" placeholder="amount"></el-input>
+                </el-form-item>
+                <el-form-item prop="address">
+                    <el-input v-model="ruleForm.address" placeholder="address"></el-input>
+                </el-form-item>
+                <el-form-item prop="desc">
+                    <el-input v-model="ruleForm.desc" placeholder="desc"></el-input>
+                </el-form-item>
+                <div class="ret-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">发起捐赠</el-button>
+                </div>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+    var baseUrl = 'http://localhost:8089/';
+    if(process.env.NODE_ENV === 'production'){
+       baseUrl='/';
+    }
+    export default {
+        data: function(){
+            return {
+                ruleForm: {
+                    amount: '',
+                    address: '',
+                    desc: '',
+                },
+                rules: {
+                    amount: [
+                        { required: true, message: '请输入捐赠金额', trigger: 'blur' }
+                    ],
+                    address: [
+                        { required: true, message: '请输入钱包地址', trigger: 'blur' }
+                    ],
+                    desc: [
+                        { required: true, message: '请输入捐款描述', trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            submitForm(formName) {       
+                const self = this;
+                self.$refs[formName].validate(function(valid){
+                    if(valid){
+                     console.log('验证成功');
+                     let amount=self.ruleForm.amount;
+                     let address =self.ruleForm.address;
+                     let desc=self.ruleForm.desc;
+                     let url='https://wfc.xyblock.net/#/wifiPortal/donate'+'?toAmount='+amount+'&toAddress='+address+'&payTitle='+desc;
+                     let vfurl=baseUrl+'checkaddr/'+address;
+                   self.$axios.get(vfurl).then((res) => {
+                   console.log("GET-"+res.data.code);
+	           console.log("GET-"+res.data.uid);
+		   console.log("GET-"+res.data.msg);
+                   if(res.data.code===200){
+                     console.log(url);
+                     window.location= url;
+                   }else
+                   {
+                      alert(res.data.msg)
+                   }
+                   })
+                     }else{
+                       return false;
+                       console.log('验证失败');
+                     }
+                 });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login-wrap{
+        position: relative;
+        width:100%;
+        height:100%;
+    }
+    .ms-title{
+        position: absolute;
+        top:50%;
+        width:100%;
+        margin-top: -230px;
+        text-align: center;
+        font-size:30px;
+        color: #fff;
+
+    }
+    .ms-login{
+        position: absolute;
+        left:50%;
+        top:50%;
+        width:280px;
+        height:240px;
+        margin:-150px 0 0 -180px;
+        padding:40px;
+        border-radius: 5px;
+        background: #fff;
+    }
+    .ret-btn button{
+        text-align: center;
+	position: relative;
+        width:100%;
+        margin:0px 0px 5px 0px;
+        height:35px;
+    }
+</style>
---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bitcoin = require('bitcoin');
+var StringBuffer = require("stringbuffer");
+var redism=require("redis");
+var uuidv1 = require('uuid/v1');
+var timeout =3600;
+var serport=8089;
+var client = new bitcoin.Client({
+  host: 'localhost',
+  port: 9665,
+  user: 'test',
+  pass: 'admin',
+  timeout: 30000
+});
+function setcor(res) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+}
+app.use(express.static(path.resolve(__dirname, './dist')))
+
+app.get('/', function(req, res) {
+    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+    res.send(html)
+})
+//根据addr地址验证此地址是否是合法的wfc地址
+app.get('/checkaddr/:addr', function(req, res){
+ setcor(res);
+ var wdaddr=req.params.addr;
+ client.validateAddress(wdaddr, function(err, txid, resHeaders) {
+                    if (txid.isvalid) {
+                            var err = {};
+                            err.code = 200;
+                            err.msg = "WFC地址正确"
+                            res.json(err);
+                    } else {
+                        var err = {};
+                        err.code = 901;
+                        err.msg = "WFC地址不正确"
+                        res.json(err);
+                    }
+  });
+})
+var server = app.listen(serport, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('listening at http://%s:%s', host, port);
+});
